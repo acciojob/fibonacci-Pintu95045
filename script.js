6edac863695f26fdf1bccd630a2a5ee22c3ab80f
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-08-12 19:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 1) return 0; // The 1st Fibonacci number is 0
+    if (num === 2) return 1; // The 2nd Fibonacci number is 1
     
     let a = 0, b = 1, next;
     
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) { // Start from the 3rd Fibonacci number
         next = a + b;
         a = b;
         b = next;
