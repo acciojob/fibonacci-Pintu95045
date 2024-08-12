f7a93b88399638319c4edd6fb7a65fa8cd5bb22b
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-08-12 18:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 function fibonacci(num) {
 // your code here
-	if (n <= 0) throw new ERROR("Input must be a positive integer");
-    if (n == 1) return 0;
-	if (n == 2) return 1;
-
-    let prev2 = 0;
-    let prev1 = 1;
-
-    for (let i = 3; i <= n; i++) {
-        const current = prev1 + prev2;
-        prev2 = prev1;
-        prev1 = current;
-    }
-    return prev1;
-	
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	return fibonacci(n-1)+ fibonacci(n-2);
 }
 
 module.exports = fibonacci;
